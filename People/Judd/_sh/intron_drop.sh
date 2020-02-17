4ecfbf91edf3f7996dabc3dafbfaea9cd092571f
abearab <abardesigner@gmail.com>
2020-02-17 14:31:35 -0800

    Feb.17

--- People/Judd/_sh/intron_drop.sh
+++
@@ -1,3 +0,0 @@
-python /rumi/shams/abe/Workflows/meRIPseq/intron_drop.py \
--i /rumi/shams/abe/People/Judd \
--s hg38 -m control
--- People/Judd/_sh/meRIPseq.sh
+++
@@ -1,3 +0,0 @@
-Rscript /rumi/shams/abe/Workflows/meRIPseq/main.R \
--i /rumi/shams/abe/People/Judd \
--s HIV -e METTL3,METTL14,Virma,WTAP
