934ff5d04d95f87a7bd03a047a0a19ff7e614d28
abardesigner <abardesigner@gmail.com>
2020-12-10 02:35:44 +0000

    Update Decitabine 2.1!
    Add screen data and results.

---
+++ Gilbertlab/Decitabine_treatment/RNA-seq/hl60-stbl/README.md
@@ -0,0 +1,4 @@
+Tools:
+- https://github.com/csglab/REMBRANDTS
+- https://www.bioconductor.org/packages/release/bioc/html/limma.html
+- https://github.com/goodarzilab/PAGE
--- my_scripts/loop_featureCounts.sh
+++ my_scripts/loop_featureCounts.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #loop featureCounts intron and exon counts
-bam_folder=$1
-stbl_folder=$2
 
 set -o errexit
 set -o nounset
 set -o xtrace
 
+bam_folder='/rumi/shams/abe/Gilbertlab/Decitabine_treatment/RNA-seq/hl60-bam'
+stbl_folder='/rumi/shams/abe/Gilbertlab/Decitabine_treatment/RNA-seq/hl60-stbl'
 GTF_index='/rumi/shams/genomes/hg38/hg38_ensemble_'
 
 main() {
@@ -15,8 +15,8 @@ main() {
     base=`basename $f`
     sample=${base/.bam/};
     echo -e '----------------------- ' $sample  ' -----------------------'
-    echo featureCounts -M -T 18 -t intron -g gene_id -a ${GTF_index}introns.gtf -o ${stbl_folder}/counts/${sample}_introns.txt ${f}
-    echo featureCounts -M -T 18 -t exon -g gene_id -a ${GTF_index}consExons.gtf -o ${stbl_folder}/counts/${sample}_exons.txt ${f}
+    featureCounts -M -T 18 -t intron -g gene_id -a ${GTF_index}introns.gtf -o ${stbl_folder}/counts/${sample}_introns.txt ${f}
+    featureCounts -M -T 18 -t exon -g gene_id -a ${GTF_index}consExons.gtf -o ${stbl_folder}/counts/${sample}_exons.txt ${f}
   done
 }
 
